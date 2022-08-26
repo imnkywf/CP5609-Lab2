d2d141afa2cd4d29012f6ae2bccbfbf9d495ff18
Imnkywf <yiwei.fan@my.jcu.edu.au>
2022-08-26 13:24:43 +0800

    Add System default files and Makefile list.

---
+++ cmake-build-debug/CMakeFiles/CP5609-Lab2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CP5609-Lab2.dir/hello.cpp.obj"
+  "CMakeFiles/CP5609-Lab2.dir/hello.cpp.obj.d"
+  "CP5609-Lab2.exe"
+  "CP5609-Lab2.exe.manifest"
+  "CP5609-Lab2.pdb"
+  "libCP5609-Lab2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CP5609-Lab2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
