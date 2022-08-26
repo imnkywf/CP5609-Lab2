465a14c06742f9c48716b3a1939d490ca21476cf
Imnkywf <yiwei.fan@my.jcu.edu.au>
2022-08-26 13:48:53 +0800

    Add d and obj files

---
+++ cmake-build-debug/CMakeFiles/CP5609-Lab2.dir/Cookie.cpp.obj.d
@@ -0,0 +1,4 @@
+CMakeFiles/CP5609-Lab2.dir/Cookie.cpp.obj: \
+ C:\Users\Administrator\Desktop\CP5609\Lab2\CP5609-Lab2\Cookie.cpp \
+ C:\Users\Administrator\Desktop\CP5609\Lab2\CP5609-Lab2\BoardObjectH.h \
+ C:\Users\Administrator\Desktop\CP5609\Lab2\CP5609-Lab2\CookieH.h
---
+++ cmake-build-debug/CMakeFiles/CP5609-Lab2.dir/Wall.cpp.obj.d
@@ -0,0 +1,4 @@
+CMakeFiles/CP5609-Lab2.dir/Wall.cpp.obj: \
+ C:\Users\Administrator\Desktop\CP5609\Lab2\CP5609-Lab2\Wall.cpp \
+ C:\Users\Administrator\Desktop\CP5609\Lab2\CP5609-Lab2\BoardObjectH.h \
+ C:\Users\Administrator\Desktop\CP5609\Lab2\CP5609-Lab2\WallH.h
